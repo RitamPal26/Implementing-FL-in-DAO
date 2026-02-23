ad49b4425a1427191056e0d547da2146b4cb7922
Ritam Pal <ritamjunior26@gmail.com>
2026-02-23 07:18:41 +0000

    feat: mine.js

---
+++ scripts/mine.js
@@ -0,0 +1,20 @@
+const { mineBlocks } = require("./helpers");
+
+async function main() {
+  console.log("⏩ Fast-forwarding time to bypass the DAO voting delay...");
+
+  // Most test environments set the voting delay to 1 block.
+  // We will mine 2 blocks just to be absolutely sure we pass it!
+  await mineBlocks(2);
+
+  console.log(
+    "✅ Time travel complete! Your proposal should now be Active (State 1).",
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
